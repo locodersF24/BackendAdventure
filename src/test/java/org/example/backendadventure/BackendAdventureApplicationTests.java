64ad45504eb9e93577c54eeaae0e854788fd1bf8
JosefineChristine <joro0003@stud.kea.dk>
2025-03-04 13:45:03 +0100

    first commit

---
+++ src/test/java/org/example/backendadventure/BackendAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendadventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAdventureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
