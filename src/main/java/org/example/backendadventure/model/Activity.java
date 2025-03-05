0f524aa40a7c20e9173c72c0b8b01e5aa8856b4c
zako0001 <zako0001@stud.kea.dk>
2025-03-05 11:12:01 +0100

    initial commit

---
+++ src/main/java/org/example/backendadventure/model/Activity.java
@@ -0,0 +1,57 @@
+package org.example.backendadventure.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autogenerates id
+    private int id;
+    @Column(unique=true)
+    private String name;
+    @Column(nullable = false)
+    private int maxNumberOfPeople;
+    @Column(nullable = false)
+    private int ageLimit;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "activity")
+    @JsonBackReference
+    private Set<Reservation> reservations = new HashSet<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxNumberOfPeople() {
+        return maxNumberOfPeople;
+    }
+
+    public void setMaxNumberOfPeople(int maxNumberOfPeople) {
+        this.maxNumberOfPeople = maxNumberOfPeople;
+    }
+
+    public int getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(int ageLimit) {
+        this.ageLimit = ageLimit;
+    }
+}
